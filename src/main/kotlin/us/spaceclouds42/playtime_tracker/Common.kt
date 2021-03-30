00fefd214606151aace57582a4b16914445f7087
SpaceClouds42 <spaceclouds42@gmail.com>
2021-03-30 11:36:29 -0400

    init

---
+++ src/main/kotlin/us/spaceclouds42/playtime_tracker/Common.kt
@@ -0,0 +1,10 @@
+package us.spaceclouds42.playtime_tracker
+
+import net.fabricmc.api.ModInitializer
+
+object Common : ModInitializer {
+    override fun onInitialize() {
+        println("Hello World")
+    }
+}
+
